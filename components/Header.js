5ecc7c343482b5fabc622f362c4640669db21605
Bruno Serrão <brunosserrao@gmail.com>
2019-10-31 21:45:12 +0100

    partes 2 3 e 4 do curso

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import TitleText from './TitleText';
+import Colors from '../constants/colors';
+
 const Header = props => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <TitleText>{props.title}</TitleText>
     </View>
   );
 };
@@ -12,15 +15,11 @@ const Header = props => {
 const styles = StyleSheet.create({
   header: {
     width: '100%',
-    height: 70,
-    paddingTop: 26,
-    backgroundColor: '#f7287b',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: Colors.primary,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  headerTitle: {
-    color: 'black',
-    fontSize: 18
   }
 });
 
